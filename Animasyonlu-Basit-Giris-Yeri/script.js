d0ff9d94b22c4ba40ff5f7458d70dfad4c9ad1d8
Hasyiğit Akkuş <70540055+Hasyigit@users.noreply.github.com>
2021-11-15 16:55:54 +0300

    Add files via upload

---
+++ Animasyonlu-Basit-Giris-Yeri/script.js
@@ -0,0 +1,32 @@
+function kontrol() {
+    var username, pass, text ; 
+    
+    // Kullanıcı adının uzunluğunu username değişkenine aktar
+    // Şifrenin uzunluğunu pass değişkenine aktar
+    username= document.getElementById("kullaniciadi").value.length;
+    pass= document.getElementById("sifre").value.length;
+    
+   
+    // Kullanıcı adı veya şifre girilmemişse
+    if (username == 0 || pass == 0) {    
+    text= "Kullanici adı veya sifre bos birakilamaz";
+    }
+    
+    // Şifre 6 karakterden kısaysa
+    else if (pass < 6) {
+    text= "Sifre 6 karakterden kisa olamaz";
+    }
+    
+    // Kullanıcı adı 3'den küçük 20'den fazlaysa 
+    else if (username < 3 || username > 20) {    
+    text= "Kullanici adi 3-20 karakter arasında olmalıdır"; 
+    }
+        
+    // İkisi de uygunsa
+    else {    
+    text= "Kayıt Basarili" ;
+    }
+    
+    
+    document.getElementById("result").innerHTML = text;
+}
